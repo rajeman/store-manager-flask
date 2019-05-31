e4457d954f85822ef7abcd1542f871986c473f5a
Emmanuel Raji <rojo.mosco@gmail.com>
2019-05-31 20:11:49 +0100

    ch(deploy-heroku): deploy app to heroku - deploy to heroku with gunicorn
    [Finishes: #166386596]

--- config.py
+++ config.py
@@ -7,18 +7,20 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     load_dotenv(os.path.join(basedir, '.env'))
     SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+    JWT_ACCESS_TOKEN_EXPIRES = 3600 * 72
 
 
 class DevelopmentConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.getenv('DEV_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = True
-    JWT_ACCESS_TOKEN_EXPIRES = 3600 * 72
+
 
 
 class ProductionConfig(Config):
     DEBUG = False
-
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class TestingConfig(Config):
     Testing = True
